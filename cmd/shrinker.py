8036b1d2a20a6e8e29fe41ba8ba84bae7b6a1cde
Luz Cazares <luz.cazares@intel.com>
2018-01-31 01:08:48 +0000

    Initial parser commands

---
+++ cmd/shrinker.py
@@ -0,0 +1,371 @@
+"""
+shrinker is a CLI providing few commands to shrink the size of a text or log
+file.
+
+Available sub-commands:
+
+---------------------
+shrinklog sub-command:
+---------------------
+
+Extracts a section of a custom log by removing anything
+before a given initSectionStr and removing anything after
+a given endSectionStr.
+Removes additional lines from the top and bottom of the output.
+
+Arguments
+~~~~~~~~~
+
+* ``--root-dir, -d``: The parent directory where files to be parsed lives. 
+  Folder can contain sub-folders.
+* ``--initstr, -i``: All lines before (and including) initstr regex will be
+  removed from outcome file.
+* ``--endstr, -e``: All lines after (and including) endstr regex will be
+  removed from outcome file.
+* ``--remove-from-top, -t``: (Optional) Removes t number of lines from the
+  top of the outcome file.
+* ``--remove-from-bottom, -b``: (Optional) Removes b number of lines from the
+  bottom of the outcome file.
+ 
+Usage
+~~~~~
+
+shrinklog takes a parent folder which may contain sub-folders and
+text file(s) to be parsed.
+At the same level of parent folder, shrinklog creates ParsedFiles folder to
+hold all the files parsed.
+
+----------------------------
+remove-from-top sub-command:
+----------------------------
+
+Removes N number of lines from the top of the file.
+
+Arguments
+~~~~~~~~~
+
+* ``--number, -n``: The number of lines to be removed.
+
+Usage
+~~~~~
+
+remove-from-top takes an int argument for the number of lines to
+remove from the top.
+
+-------------------------------
+remove-from-bottom sub-command:
+-------------------------------
+
+Removes N number of lines from the bottom of the file.
+
+Arguments
+~~~~~~~~~
+
+* ``--number, -n``: The number of lines to be removed.
+
+Usage
+~~~~~
+
+remove-from-bottom takes an int argument for the number of lines to
+remove from the bottom.
+
+------------------------------
+remove-from-regex sub-command:
+------------------------------
+
+Removes the lines which matches the given regex(es)
+
+Arguments
+~~~~~~~~~
+
+* ``--regex, -r``: The regex(es) to remove from the file..
+
+Usage
+~~~~~
+
+remove-from-regex takes a regex or several regexes to delete the lines that
+matches the given regex(es).
+
+"""
+import argparse
+import os
+import re
+import sys
+import time
+import shutil
+
+from cliff import command
+from datetime import datetime
+import pdb
+
+
+class ArgumentParser():
+    def __init__(self, args=None, parser=None):
+        desc = 'shrinker provide sub-commands to shrink a text or log file.'
+        usage = ('shrinker [-h] <SUB-COMMAND> ...\n\n'
+           'To see help on a specific sub-command, do:\n'
+           'shrinker <SUB-COMMAND> --help\n\n')
+        if not parser:
+            parser = argparse.ArgumentParser(
+                description=desc,
+                usage=usage
+            )
+        self.parser = parser
+
+        subparsers = parser.add_subparsers(help='\nAvailable sub-commands.\n')
+
+        # Add arguments that goes with all sub-commands
+        common_args = self.add_common_args()
+
+        # Add sub-command parsers:
+        self.add_shrinklog_parser(subparsers, [common_args])
+        self.add_remove_from(subparsers, [common_args])
+        self.add_remove_from(subparsers, [common_args], 'bottom')
+        self.add_remove_from_regex(subparsers, [common_args])
+
+    def parse_args(self, args):
+        return self.parser.parse_args(args=args)
+
+    def add_common_args(self):
+        '''Common arguments to all subcommands of shrinker cli.'''
+        # Arguments that go with all subcommands.
+        shared_args = argparse.ArgumentParser(add_help=False)
+        #group = shared_args.add_mutually_exclusive_group()
+        #group.add_argument('-s', '--silent',
+        #                   action='store_true',
+        #                   help='Suppress output except warnings and errors.')
+        #group.add_argument('-v', '--verbose',
+        #                   action='count',
+        #                   help='Show verbose output.')
+        shared_args.add_argument(
+            "-d", "--root-dir", metavar="<dir_name>",
+            action='store', required=True, dest='root_dir', type=str,
+            help="The root directory where text file(s) are stored.")
+        shared_args.add_argument(
+            "-o", "--output-dir", metavar="<dir_name>",
+            action='store', required=False, dest='output_dir', type=str,
+            default =  'ParsedFiles',
+            help="The directory where parsed file(s) will be saved.")
+        shared_args.add_argument(
+            "--inline", action='store_true',
+            required=False,
+            help='Asume parsed file must replace existing original one.'
+        )
+
+        # Return common args
+        return shared_args
+ 
+    def add_shrinklog_parser(self, subparsers, parents=None):
+        '''Arguments for shrinklog subcommand.'''
+        desc = "subcommand extracts a portion of a custom text or log file."
+        parser_sl = subparsers.add_parser('shrinklog', parents=parents,
+                                          help=desc)
+        parser_sl.add_argument(
+            "-i", "--initstr", metavar="<regex>",
+            action='store', required=True, type=str,
+            help="String where program start extracting data.")
+
+        parser_sl.add_argument(
+            "-e", "--endstr", metavar="<regex>",
+            action='store', required=True, type=str,
+            help="String where program stop extracting data.")
+
+        parser_sl.add_argument(
+            "-t", "--remove-from-top", metavar="N",
+            action='store', dest="remove_top", default=0, type=int,
+            help="Additional lines to remove from the top.")
+
+        parser_sl.add_argument(
+            "-b", "--remove-from-bottom", metavar="N",
+            action='store', dest="remove_bottom", default=0, type=int,
+            help="Additional lines to remove from the bottom.")
+
+        parser_sl.set_defaults(func="shrink")
+
+    def add_remove_from(self, subparsers, parents=None, pos='top'):
+        '''Arguments for remove-from-top and remove-from-bottom subcommands.'''
+        desc = ("subcommand remove N lines from the %s of the file." % pos)
+        parser_rf = subparsers.add_parser(('remove-from-%s' % pos),
+                                           parents=parents, help=desc)
+        parser_rf.add_argument(
+            "-n", "--number", metavar="<number>",
+            action='store', required=True, type=int,
+            help="Number of lines to delete.")
+
+        parser_rf.set_defaults(func='remove_from', func2=pos)
+
+    def add_remove_from_regex(self, subparsers, parents=None):
+        '''Arguments for remove-from-regex subcommand.'''
+        desc = "Remove lines from a file, when it matches given regex(es)."
+        parser_rf = subparsers.add_parser('remove-from-regex',
+                                          parents=parents,
+                                          help=desc)
+        parser_rf.add_argument(
+            "-r", "--regex", metavar="<regex>",
+            action='store', required=True,
+            type=str, nargs='*',
+            help="Regex(ex) to look for on the files to parse.")
+
+        parser_rf.set_defaults(func='remove_from', func2='regex')
+
+
+class Shrinker():
+    def __init__(self, args):
+        self.args = args
+        if not os.path.isdir(args.root_dir):
+            raise Exception("You must provide a valid root folder.")
+
+        output_dir = args.output_dir
+        if args.inline:
+            output_dir = '/tmp/ParsedFiles'+datetime.now().isoformat()
+            self.args.output_dir = output_dir
+        if os.path.abspath(args.root_dir) == os.path.abspath(output_dir):
+            raise Exception("Input and Output folders must be different.")
+
+        # Create output folder if exists create a backup
+        if os.path.exists(output_dir):
+            shutil.move(output_dir, output_dir+'.bk.'+datetime.now().isoformat())
+        os.makedirs(output_dir)
+
+    def shrink(self):
+        '''Main function for shinklog subcommand'''
+        # Get and process files
+        count = 0
+        for root, _, files in os.walk(self.args.root_dir):
+            for filename in files:
+                origin = os.path.join(root, filename)
+                output = os.path.join(self.args.output_dir, filename)
+                count = count + 1
+                # Remove content before initStr
+                data = self._remove_before(origin, self.args.initstr)
+                # Remove N lines from the top
+                data = self._remove_lines(data, self.args.remove_top + 1)
+                # Remove content after endStr
+                data = self._remove_after(data, self.args.endstr)
+                # Remove M lines from the bottom
+                data = self._remove_lines(data, self.args.remove_bottom, False)
+                self._write_to_file(output, data)
+
+        # Handle inline parameter
+        self._ifinline()
+        print("Found %i files." % count)
+
+    def remove_from(self):
+        '''Main function for remove-from-[top|bottom|regex] subcommands.'''
+        # Get and process files
+        count = 0
+        for root, _, files in os.walk(self.args.root_dir):
+            for filename in files:
+                origin = os.path.join(root, filename)
+                output = os.path.join(self.args.output_dir, filename)
+                count = count + 1
+                if self.args.func2 == 'top':
+                    data = self._remove_lines(origin, self.args.number)
+                elif self.args.func2 == 'bottom':
+                    data = self._remove_lines(origin, self.args.number, False)
+                elif self.args.func2 == 'regex':
+                    data = self._remove_lines_r(origin, self.args.regex)
+                self._write_to_file(output, data)
+
+        # Handle inline parameter
+        self._ifinline()
+        print("Found %i files." % count)
+        
+    ##### Internal methods - To be used by the subcommands #####
+    def _get_content(self, name):
+        '''Get the content from a file or from stream.'''
+        if os.path.exists(name):
+            the_file = open(name, 'r')
+            content = the_file.read()
+            the_file.close()
+        else:
+            content = name
+        return content
+
+    def _locate_match(self, name, pattern):
+        '''Locate a string patern whithin a file or stream.'''
+        content = self._get_content(name)
+        return re.split(pattern, content, 1)
+
+    def _write_to_file(self, file_name, plain_content):
+        '''Writes a given text into a file.'''
+        if not plain_content:
+            print ("<-- Error: Empty processed data for file %s" % file_name)
+            plain_content = " "
+
+        the_file = open(file_name, "w")
+        the_file.write(plain_content)
+        the_file.close()
+
+    def _ifinline(self):
+        '''Replaces the original folder with parsed output when inline
+           parameter is passed'''
+        # If parsed file should replace original files
+        if self.args.inline:
+            shutil.rmtree(self.args.root_dir)
+            shutil.move(self.args.output_dir, self.args.root_dir)
+
+    def _remove_before(self, name, initstr):
+        '''Remove lines from the top of a file or stream until 
+           given string regex matches'''
+        content = self._locate_match(name, initstr)
+        if len(content) > 1:
+            return content[1]
+        else:
+            return content[0]
+
+    def _remove_after(self, name, endstr):
+        '''Remove the lines from a file or stream after a 
+           given string regex matches'''
+        content = self._locate_match(name, endstr)
+        return content[0]
+
+    def _remove_lines(self, name, num, top=True):
+        '''Remove num lines from the top or bottom of a given file or stream'''
+        content = self._get_content(name)
+        if top:
+            return re.split('\n', content, num)[num]
+        else:
+            content = content[::-1]
+            content = re.split('\r', content, num)[num]
+            return content[::-1]
+
+    def _remove_lines_r(self, name, regex):
+        '''Remove the lines that match a given regex.'''
+        content = self._get_content(name)
+        count = len(content.split('\n'))
+        for r in regex:
+            content = re.sub('.*'+r+'.*\n', '', content)
+        count = count - len(content.split('\n'))
+        print ('Removed %i lines.' % count)
+        return content
+
+
+# CLIFF CLI CREATOR CLASS
+class CliffShrinker(command.Command):
+    '''Shrinker commands'''
+
+    def get_parser(self, prog_name):
+        parser = super(CliffShrinker, self).get_parser(prog_name)
+        ArgumentParser(None, parser)
+        return parser
+
+    def take_action(self, parsed_args):
+        main(parsed_args)
+
+
+def main(opts=None):
+    # Parse arguments
+    if opts is None:
+        opts = ArgumentParser().parse_args(opts)
+    # Create commands instance
+    shrinker = Shrinker(opts)
+    # Run parsed subcommand function
+    raise SystemExit(getattr(shrinker, opts.func)())
+
+
+if __name__ == "__main__":
+    start_time = time.time()
+    main()
+    print("--- %s seconds ---" % (time.time() - start_time))
+
